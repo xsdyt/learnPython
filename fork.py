2bbf620a9a659205830250cfa42ead6c03137ab7
amos <amos@my.com>
2017-03-22 18:03:38 +0800

    0322 -2

--- fork.py
+++ fork.py
@@ -57,13 +57,35 @@ from multiprocessing import Pool
   # 很多时候子进程并不是自身，而是一个外部地址。创建子进程，还需要控制子进程的输入和输出
 #subprocess模块可以让我们非常方便的启动进程，然后控制输入和输出
 
-import subprocess
-print('$ nslookup www.python.org')
-r=subprocess.call(['nslookup','www.python.org'])
-print('Exit code:',r)
+# import subprocess
+# print('$ nslookup www.python.org')
+# r=subprocess.call(['nslookup','www.python.org'])
+# print('Exit code:',r)
+#
+# print('$ nslookup')
+# p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
+# # print(output.decode('utf-8'))
+# print('Exit code:', p.returncode)
 
-print('$ nslookup')
-p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
-# print(output.decode('utf-8'))
-print('Exit code:', p.returncode)
+from multiprocessing import Queue
+
+def write(q):
+  print('wtite process%s' %os.getpid())
+  for i in [1,2,3,4,5]:
+    print('put %s queue' %i)
+    q.put(i)
+    time.sleep(random.random())
+def read(q):
+  print('read process %s' %os.getpid())
+  while True:
+    value=q.get(True)
+    print('get %s queue' %value)
+if __name__=="__main__":
+  q=Queue()
+  pw=Process(target=write,args=(q,))
+  pr=Process(target=read,args=(q,))
+  pw.start()
+  pr.start()
+  pw.join()
+  pr.terminate()#读取时死循环，所以需要强制停止
